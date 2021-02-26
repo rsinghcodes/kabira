921670947b36a4852c721b4428303930ab72ec3a
Raghvendra Singh <raghvendrapratap.success@gmail.com>
2021-02-26 22:35:40 +0530

    deleted old components and add new components

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,43 @@
-import React from "react";
 import Head from "next/head";
-import Header from "../components/Header";
-import styled from "styled-components";
-import ServiceBox from "../components/ServiceBox";
-import ImgBox from "../components/ImgBox";
-
-const Banner = styled.div`
-  position: relative;
-  width: 100%;
-  min-height: 80vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: url(../1.jpg);
-  background-size: cover;
-`;
-
-const Container = styled.div`
-  position: relative;
-  padding: 0 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  @media screen and (max-width: 700px) {
-    padding: 0 1rem;
-  }
-`;
-
-const Title = styled.h2`
-  font-weight: bold;
-  letter-spacing: 1px;
-  margin-bottom: 0;
-  font-size: 2rem;
-  @media screen and (max-width: 700px) {
-    font-size: 1.3rem;
-  }
-`;
-
-const SubTitle = styled.p`
-  font-weight: 300;
-  margin-top: 0.4rem;
-  margin-bottom: 1rem;
-  @media screen and (max-width: 700px) {
-    font-size: 0.8rem;
-    text-align: center;
-  }
-`;
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Kabira | Home</title>
-      </Head>
-      <Header />
-      <Banner />
-      <Container>
-        <ServiceBox />
-        <Title>Our Furniture Works</Title>
-        <SubTitle>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </SubTitle>
-        <ImgBox />
-        <Title>Our Interior Works</Title>
-        <SubTitle>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </SubTitle>
-      </Container>
+      <header className={utilStyles.header}>
+        <h1 className={utilStyles.headingXl}>Kabira</h1>
+        <ul className={utilStyles.list}>
+          <li className={utilStyles.listItem}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className={utilStyles.listItem}>
+            <Link href="/">
+              <a>Our Services</a>
+            </Link>
+          </li>
+          <li className={utilStyles.listItem}>
+            <Link href="/">
+              <a>About us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </header>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        {/* <Image src="/1.jpg" width="100%" height="100%" /> */}
+      </Layout>
     </>
   );
 }
